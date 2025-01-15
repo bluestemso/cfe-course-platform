4416f35b3a06433859bec550c0f0af448932b9dc
taylor <taylor@bluestem.solutions>
2025-01-15 16:57:44 -0500

    Update project configuration and add course model features
    - Added 'local_cdn/' to .gitignore.
    - Updated requirements.txt to include Pillow, cloudinary, and python-decouple.
    - Configured MEDIA_URL and MEDIA_ROOT in settings.py for media file handling.
    - Updated urls.py to serve media files in DEBUG mode.
    - Registered Course model in admin.py and modified models.py to use CloudinaryField for image uploads.
    - Changed access requirement choices in Course model and adjusted field lengths.

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,4 +1,8 @@
+import helpers
 from django.db import models
+from cloudinary.models import CloudinaryField
+
+helpers.cloudinary_init()
 
 # Create your models here.
 class PublishStatus(models.TextChoices):
@@ -7,21 +11,25 @@ class PublishStatus(models.TextChoices):
     DRAFT = 'draft', 'Draft'
 
 class AccessRequirement(models.TextChoices):
-    FREE = 'free', 'Free'
-    PREMIUM = 'premium', 'Premium'
+    ANYONE = 'any', 'Anyone'
+    EMAIL_REQUIRED = 'email', 'Email Required'
+
+def handle_upload(instance, filename):
+    return f'{filename}'
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='courses/images/', blank=True, null=True)
+    # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     status = models.CharField(
-        max_length=10, 
+        max_length=12, 
         choices=PublishStatus.choices, 
         default=PublishStatus.DRAFT)
     access = models.CharField(
-        max_length=10, 
+        max_length=5, 
         choices=AccessRequirement.choices, 
-        default=AccessRequirement.FREE)
+        default=AccessRequirement.EMAIL_REQUIRED)
     
     @property
     def is_published(self):
